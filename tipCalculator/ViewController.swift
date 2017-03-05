99dfb99db0c94e8615f7fd7e275ada7efd422fb7
Douglas Hewitt <doughewitt4@gmail.com>
2017-03-04 19:38:45 -0800

    tip working

--- tipCalculator/ViewController.swift
+++ tipCalculator/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var billTextField: UITextField!
+    
+    @IBOutlet weak var tipLabel: UILabel!
+    
+    @IBOutlet weak var totalLabel: UILabel!
+    
+    @IBOutlet weak var tipPercentageSegmentedControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -20,6 +29,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func isEditingBill(_ sender: UITextField) {
+        updateLabels(text: sender.text)
+    }
+    
+    @IBAction func didChangeTipPercentage(_ sender: UISegmentedControl) {
+        updateLabels(text: billTextField.text)
+    }
+    
+    func tipPercentageFromSegmentedControl(_ segment: UISegmentedControl) -> Double {
+        let tipTitle = segment.titleForSegment(at: segment.selectedSegmentIndex)
+        guard let tipTitleFiltered = tipTitle?.replacingOccurrences(of: "%", with: "") else {return 0}
+        guard let tipValue = Double(tipTitleFiltered) else {return 0}
+        let tipPercentage = tipValue * 0.01
+        return tipPercentage
+    }
+    
+    func updateLabels(text: String?) {
+        guard let text = text else {return}
+
+        let bill = Double(text) ?? 0
+        let tipPercentage = tipPercentageFromSegmentedControl(tipPercentageSegmentedControl)
+        let tip = bill * tipPercentage
+        let total = bill + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
+    }
 
 }
 
